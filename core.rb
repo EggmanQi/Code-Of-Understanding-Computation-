df719005987f5ef8dc6132b59e8ec118027f89be
edwin.qi <edwin.qi@vipshop.com>
2020-04-07 22:24:46 +0800

    小步语义代码练习结束

--- core.rb
+++ core.rb
@@ -1,12 +1,12 @@
-# 定义规约: 输入的代码是否能进行规约, 可理解为代码能否进行求值, 在 demo 中 Number 类即不能求值
-def reducible?(expression)
-    case expression
-        when Number
-            false
-        when Add, Mutiply
-            true
-    end
-end
+# 定义规约: 输入的代码是否能进行规约, 可理解为代码能否进行求值,  demo 中 Number 类即不能求值
+#def reducible?(expression)
+#    case expression
+#        when Number
+#            false
+#        when Add, Mutiply
+#            true
+#    end
+#end
 
 # 构造虚拟机: 代码和状态封装到一个类, 对表达式进行求值(小步规约) 
 class Machine < Struct.new(:statement, :envirioment)
@@ -185,8 +185,54 @@ class If < Struct.new(:condition, :consequence, :alternative)
                 when Boolean.new(true)
                     [consequence, envirioment]
                 when Boolean.new(false)
-                    [alternative. envirioment]
+                    [alternative, envirioment]
             end
+        end
+    end
+end
+
+# 定义循环
+class While < Struct.new(:condition, :body)
+    def to_s
+        "while (#{condition}) {#{body}}"
+    end
+
+    def inspect
+        "#{self}"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce(envirioment)
+        [If.new(condition, 
+                Sequence.new(body, self), DoNothing.new), 
+                envirioment]
+    end
+end
+
+# 定义序列
+class Sequence < Struct.new(:first, :second) 
+    def to_s
+        "#{first}; #{second}"
+    end
+
+    def inspect
+        "#{self}"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce(envirioment)
+        case first
+        when DoNothing.new
+            [second, envirioment]
+        else 
+            reduce_first, reduce_envirioment = first.reduce(envirioment)
+            [Sequence.new(reduce_first, second), reduce_envirioment]
         end
     end
 end
@@ -223,25 +269,42 @@ end
 #    { x: Number.new(2)}
 #).run
 
+#Machine.new(
+#    If.new(
+#        Variable.new(:x),
+#        Assign.new(:y, Number.new(1)),
+#        Assign.new(:y, Number.new(2))
+#    ),
+#    { x: Boolean.new(true)}
+#).run
+
+
+#Machine.new(
+#    If.new(
+#        Variable.new(:x),
+#        Assign.new(:y, Number.new(3)),
+#        DoNothing.new
+#    ),
+#    { x: Boolean.new(false)}
+#).run
+
+puts "  "
+
+#Machine.new(
+#    Sequence.new(
+#        Assign.new(:x, Add.new(Number.new(1), Number.new(3))),
+#        Assign.new(:y, Mutiply.new(Variable.new(:x), Number.new(3)))
+#    ),
+#    {}
+#).run
+
 Machine.new(
-    If.new(
-        Variable.new(:x),
-        Assign.new(:y, Number.new(1)),
-        Assign.new(:y, Number.new(2))
+    While.new(
+        LessThan.new(Variable.new(:x), Number.new(5)),
+        Assign.new(:x, Mutiply.new(Variable.new(:x), Number.new(3)))
     ),
-    { x: Boolean.new(true)}
+    { x: Number.new(1)}
 ).run
-
-
-Machine.new(
-    If.new(
-        Variable.new(:x),
-        Assign.new(:y, Number.new(3)),
-        DoNothing.new
-    ),
-    { x: Boolean.new(false)}
-).run
-
 
 
 
